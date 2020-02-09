b66b2e642d78321346f0464eaef62be923d6bf34
Kevin <metrix.28@gmail.com>
2020-02-09 11:05:27 -0500

    test commit 3

--- app/src/main/java/com/lotex/android/simplepasswordgenerator/MainActivity.java
+++ app/src/main/java/com/lotex/android/simplepasswordgenerator/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ///testing
-        ///testing 2
+        //testing
+        //testing 2
     }
 }
