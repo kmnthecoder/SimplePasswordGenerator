5bcef546e2b643fedd1c6ce277fc794f1fce68aa
Kevin <metrix.28@gmail.com>
2020-02-25 15:42:47 -0500

    - refactor passwordgenerator - change onclicklistener style - add up navigation and add back action bar

--- app/src/main/java/com/lotex/android/simplepasswordgenerator/Activities/MainActivity.java
+++ app/src/main/java/com/lotex/android/simplepasswordgenerator/Activities/MainActivity.java
@@ -1,19 +1,15 @@
 package com.lotex.android.simplepasswordgenerator.Activities;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
 import android.widget.Button;
-
 
 import com.lotex.android.simplepasswordgenerator.R;
 
-public class MainActivity extends AppCompatActivity implements
-        View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
@@ -26,7 +22,6 @@ public class MainActivity extends AppCompatActivity implements
         // Set views to variables
         viewAttach();
     }
-
 
     public void viewAttach() {
         mPasswordGenerator = findViewById(R.id.button_password_generator);
@@ -48,6 +43,5 @@ public class MainActivity extends AppCompatActivity implements
                 break;
         }
     }
-
 
 } // class end
