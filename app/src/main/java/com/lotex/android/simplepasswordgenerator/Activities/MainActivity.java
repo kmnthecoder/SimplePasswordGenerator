c70ef9644b0b46037d9534b9574aeccc53a58687
Kevin <metrix.28@gmail.com>
2020-07-09 16:23:13 -0400

    Revert "- refactor passwordgenerator"
    This reverts commit 5bcef546e2b643fedd1c6ce277fc794f1fce68aa.

--- app/src/main/java/com/lotex/android/simplepasswordgenerator/Activities/MainActivity.java
+++ app/src/main/java/com/lotex/android/simplepasswordgenerator/Activities/MainActivity.java
@@ -1,15 +1,19 @@
 package com.lotex.android.simplepasswordgenerator.Activities;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
 import android.widget.Button;
+
 
 import com.lotex.android.simplepasswordgenerator.R;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements
+        View.OnClickListener {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
@@ -22,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // Set views to variables
         viewAttach();
     }
+
 
     public void viewAttach() {
         mPasswordGenerator = findViewById(R.id.button_password_generator);
@@ -43,5 +48,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
     }
+
 
 } // class end
--- app/src/main/java/com/lotex/android/simplepasswordgenerator/Activities/PasswordGenerator.java
+++ app/src/main/java/com/lotex/android/simplepasswordgenerator/Activities/PasswordGenerator.java
@@ -24,7 +24,7 @@ import com.lotex.android.simplepasswordgenerator.R;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class PasswordGenerator extends AppCompatActivity implements View.OnClickListener {
+public class PasswordGenerator extends AppCompatActivity {
 
     private static final String LOG_TAG = PasswordGenerator.class.getSimpleName();
 
@@ -34,30 +34,84 @@ public class PasswordGenerator extends AppCompatActivity implements View.OnClick
     private Spinner numSpinner;
     private CheckBox mUpperLower, mSymbols, mNumbers;
     private EditText mExclude;
-    private Button mGenerateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_generator);
-        // Set views to variables
-        viewAttach();
+
+        // Set on click listener for generate password button
+        addListenerGenerate();
+        // Set on click listener for copy and pasting the generated password
+        passwordTextCopy();
         // Set up spinner and populate options
         spinnerSetup();
+        // Set views to variables
+        viewAttach();
+
     }
+
+    // Add listener to generate password button
+    public void addListenerGenerate() {
+
+        // Set view to variable
+        Button mGenerateButton = findViewById(R.id.button_generate);
+        // Set on click listener for generate password button
+        mGenerateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Check if there is a number selection
+                if (numSpinner.getSelectedItemPosition() != 0) {
+
+                    //Toast.makeText(getApplicationContext(), "Generating Password", Toast.LENGTH_SHORT).show();
+
+                    /*
+                    Log.d(LOG_TAG, "mUpperLower is checked: " + mUpperLower.isChecked() +
+                            "\nmSymbols is checked: " + mSymbols.isChecked() +
+                            "\nmNumbers is checked: " + mNumbers.isChecked() +
+                            "\nPassword length is: " + numSpinner.getSelectedItem().toString());
+
+                     */
+                    //+ "\nPassword generated is: " + generatePassword());
+
+                    mTextPassword.setText(generatePassword());
+
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please select password length", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    } // addListenerGenerate end
 
     // Copies password when password text itself is tapped
     public void passwordTextCopy() {
-        // Copy text to universal android clipboard
-        mClipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-        String text = mTextPassword.getText().toString();
-        mClip = ClipData.newPlainText("text", text);
-        mClipboard.setPrimaryClip(mClip);
-        Toast.makeText(getApplicationContext(), "Text Copied", Toast.LENGTH_SHORT).show();
-    }
+
+        // Set view to variable
+        mTextPassword = findViewById(R.id.text_password);
+
+        mTextPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Copy text to universal android clipboard
+                mClipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+                String text = mTextPassword.getText().toString();
+                mClip = ClipData.newPlainText("text", text);
+                mClipboard.setPrimaryClip(mClip);
+
+                // Toast to say text copied
+                Toast.makeText(getApplicationContext(), "Text Copied", Toast.LENGTH_SHORT).show();
+            }
+        });
+    } // passwordTextCopy end
 
     // Set up spinner and populate with options
     public void spinnerSetup() {
+
+        // Set view to variable
+        numSpinner = findViewById(R.id.spinner_passwordlength);
+
         ArrayList<String> spinnerOptions = new ArrayList<>();
         spinnerOptions.add("Select password length...");
 
@@ -95,8 +149,9 @@ public class PasswordGenerator extends AppCompatActivity implements View.OnClick
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String selectedItemText = (String) parent.getItemAtPosition(position);
+
                 // If user change the default selection
-                // First item is disabled and it is used for hint
+                // First item is disable and it is used for hint
                 if (position > 0) {
                     // Notify the selected item text
                     Toast.makeText(getApplicationContext(), "Selected : " + selectedItemText, Toast.LENGTH_SHORT).show();
@@ -109,71 +164,50 @@ public class PasswordGenerator extends AppCompatActivity implements View.OnClick
 
             }
         });
-    }
-
-    public void generatePassword() {
-        // Check if there is a number selection
-        if (numSpinner.getSelectedItemPosition() != 0) {
-
-            // Initialize variables and default random password
-            StringBuilder randomChars = new StringBuilder("abcdefghijklmnopqrstuvwxyz");
-            StringBuilder generatedPassword = new StringBuilder();
-            Random random = new Random();
-            int passwordLength = Integer.parseInt(numSpinner.getSelectedItem().toString());
-
-            // Check options, change selection of random characters if selected
-            if (mUpperLower.isChecked()) {
-                randomChars.append(randomChars.toString().toUpperCase());
-            }
-            if (mSymbols.isChecked()) {
-                randomChars.append("!@#$%^&*");
-            }
-            if (mNumbers.isChecked()) {
-                randomChars.append("0123456789");
-            }
-
-            mExclude.setText(mExclude.getText().toString().replaceAll("\\[\\]\\\\", ""));
-            if (!mExclude.getText().toString().matches("")) {
-                String rChars = randomChars.toString();
-                rChars = rChars.replaceAll("[" + mExclude.getText().toString() + "]", "");
-                randomChars = new StringBuilder(rChars);
-            }
-
-            // generate random password
-            for (int i = 0; i < passwordLength; i++) {
-                generatedPassword.append(randomChars.charAt(random.nextInt(randomChars.length())));
-            }
-            mTextPassword.setText(generatedPassword);
-        } else {
-            Toast.makeText(getApplicationContext(), "Please select password length", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.button_generate:
-                generatePassword();
-                break;
-            case R.id.text_password:
-                passwordTextCopy();
-                break;
-            default:
-                break;
-        }
-    }
+    } // spinnerSetup end
 
     public void viewAttach() {
-        mGenerateButton = findViewById(R.id.button_generate);
-        mGenerateButton.setOnClickListener(this);
-
-        mTextPassword = findViewById(R.id.text_password);
-        mTextPassword.setOnClickListener(this);
-
-        numSpinner = findViewById(R.id.spinner_passwordlength);
         mUpperLower = findViewById(R.id.checkBox_upperlower);
         mSymbols = findViewById(R.id.checkBox_symbols);
         mNumbers = findViewById(R.id.checkBox_numbers);
         mExclude = findViewById(R.id.editText_exclude);
-    }
+    } // viewAttach end
+
+    public String generatePassword() {
+        //Toast.makeText(getApplicationContext(), "Generating Password", Toast.LENGTH_SHORT).show();
+
+        // Initialize variables and default random password
+        StringBuilder randomChars = new StringBuilder("qwertyuiopasdfghjklzxcvbnm");
+        StringBuilder generatedPassword = new StringBuilder();
+        Random random = new Random();
+        int passwordLength = Integer.parseInt(numSpinner.getSelectedItem().toString());
+
+        // Check options, change selection of random characters if selected
+        if (mUpperLower.isChecked()) {
+            randomChars.append(randomChars.toString().toUpperCase());
+        }
+        if (mSymbols.isChecked()) {
+            randomChars.append("!@#$%^&*");
+        }
+        if (mNumbers.isChecked()) {
+            randomChars.append("0123456789");
+        }
+
+        mExclude.setText(mExclude.getText().toString().replaceAll("\\[\\]\\\\", ""));
+        if (!mExclude.getText().toString().matches("")) {
+            String rChars = randomChars.toString();
+            rChars = rChars.replaceAll("[" + mExclude.getText().toString() + "]", "");
+            randomChars = new StringBuilder(rChars);
+        }
+
+        // generate random password
+        for (int i = 0; i < passwordLength; i++) {
+            generatedPassword.append(randomChars.charAt(random.nextInt(randomChars.length())));
+        }
+
+        return generatedPassword.toString();
+
+    } // generatePassword end
+
+
 } // end of class
